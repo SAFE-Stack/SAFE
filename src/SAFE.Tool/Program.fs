702f5bbf9c5fcd139eee6a698d889c2341afd741
Tomasz Heimowski <tomek.heimowski@gmail.com>
2019-06-24 13:45:42 +0200

    Support fake build and run

--- src/SAFE.Tool/Program.fs
+++ src/SAFE.Tool/Program.fs
@@ -1,8 +1,22 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Diagnostics
+
+let build (target: string option) =
+    let args =
+        match target with
+        | Some target -> sprintf "build -t %s" target
+        | None -> "build"
+    let info = ProcessStartInfo("fake", args)
+    let proc = Process.Start(info)
+    proc.WaitForExit()
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello SAFE!"
+    match List.ofArray argv with
+    | [ "build" ] -> build None
+    | [ "run" ] -> build (Some "run")
+    | _ -> printfn """Usage: safe [command] 
+(Available commands: build, run)"""
     0 // return an integer exit code
